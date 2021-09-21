af0b4c2f3755b7b6b38b5d7552da080e5605197c
Geordie King <geordie.king@gmail.com>
2021-09-21 19:21:12 +0100

    make array of objects for places

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-const currLatLong = [51.3173, -0.5578]
-const NZLatLong = [-37.28695, 175.9043]
+const places = [
+          { placename: 'Woking Home', lat: 51.3173, long: -0.5578},
+          { placename: 'Whiritoa', lat: -37.28695, long: 175.9043}, 
+          { placename: 'Woking SAB', lat: 51.3184, long: -0.5622} ]
+
 
 let Stadia_AlidadeSmoothDark = L.tileLayer(
   'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
@@ -9,11 +12,11 @@ let Stadia_AlidadeSmoothDark = L.tileLayer(
 
 window.onload = function() {
   let map = L.map("mapDiv", {
-    center: currLatLong,
-    zoom: 15,
+    center: [places[0].lat,places[0].long],
+    zoom: 17,
   })
 
   map.addLayer(Stadia_AlidadeSmoothDark);
-  L.marker(currLatLong).addTo(map);
-  L.marker(NZLatLong).addTo(map);
+
+  places.forEach(p => L.marker([p.lat,p.long]).addTo(map))
 };
