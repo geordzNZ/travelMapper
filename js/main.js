0c4b6fab9061919d90f9069633b011bfb36bb097
Geordie King <geordie.king@gmail.com>
2021-09-21 20:09:10 +0100

    added what3words values to array and marker popups

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const places = [
-          { placename: 'Woking - Home', lat: 51.3173, long: -0.5578},
-          { placename: 'Whiritoa', lat: -37.28695, long: 175.9043},
-          { placename: 'Woking - ex-SAB House', lat: 51.3185, long: -0.5622},
-          { placename: 'Auckland NZ', lat: -36.85817, long: 174.6121} ]
+          { placename: 'Woking - Home', lat: 51.3173, long: -0.5578, w3w: 'solved.nests.crown'},
+          { placename: 'Whiritoa', lat: -37.28695, long: 175.9043, w3w: 'dives.perishes.beacon'},
+          { placename: 'Woking - ex-SAB House', lat: 51.3185, long: -0.5622, w3w: 'deals.tests.motor'},
+          { placename: 'Auckland NZ', lat: -36.85817, long: 174.6121, w3w: 'puts.behind.outraged'} ]
 
 let Stadia_AlidadeSmoothDark = L.tileLayer(
   'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
@@ -18,5 +18,5 @@ window.onload = function() {
 
   map.addLayer(Stadia_AlidadeSmoothDark);
 
-  places.forEach(p => L.marker([p.lat,p.long]).bindPopup(p.placename).addTo(map))
+  places.forEach(p => L.marker([p.lat,p.long]).bindPopup(`<b>${p.placename}</b> <i>///${p.w3w}</i>`).addTo(map))
 };
